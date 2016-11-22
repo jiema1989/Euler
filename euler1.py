23ff447b1c8f3dc93615023f23dd854842beb640
jiema1989 <jiema1989>
2016-11-22 01:42:27 -0700

    2016/11/22 1:42am

--- euler1.py
+++ euler1.py
@@ -1,14 +1,8 @@
 # This is used to solve Euler Problem 1#
-N=1000;
-
 def euler(number):
     sum=0;
-    for i in xrange(number):
+    for i in xrange(number+1):
         if i%3==0 or i%5==0:
             sum+=i;
     return sum;
-        
-        
-
-
 print euler(1000);
